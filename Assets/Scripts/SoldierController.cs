de2d20eb13c3030f288ee0dee6e55499ae374f5e
Ahmed Bera Pay <abera.pay@gmail.com>
2024-05-14 22:16:23 +0300

    Optimized Raycasting and Enable Kinematic for Warriors

--- Assets/Scripts/SoldierController.cs
+++ Assets/Scripts/SoldierController.cs
@@ -13,12 +13,13 @@ public class SoldierController : MonoBehaviour
     private bool isAttacking = false;
     private float currentHealth = 100f;
     private float walkDistance = 5f;
-    private RaycastHit hitInFront;
+    private Rigidbody rb;
 
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
         weapon = GetComponentInChildren<Weapon>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Start()
@@ -30,14 +31,14 @@ public class SoldierController : MonoBehaviour
     void Update()
     {
         UpdateMovement();
-        // UpdateAttack();
     }
 
     private void UpdateMovement()
     {
         if (!isAttacking)
         {
-            if (!Physics.Raycast(transform.position + Vector3.up * 2.0f, transform.forward, out hitInFront, walkDistance))
+            RaycastHit hit;
+            if (!Physics.Raycast(transform.position + Vector3.up * 2.0f, transform.forward, out hit, Mathf.Min(attackDistance, walkDistance)))
             {
                 if (!animator.GetBool("Moving"))
                     animator.SetBool("Moving", true);
@@ -45,52 +46,30 @@ public class SoldierController : MonoBehaviour
             }
             else
             {
-                // // Check if the object in front is an enemy
-                // if (((1 << hitInFront.transform.gameObject.layer) & enemyLayer) == 0)
-                // {
-                    animator.SetBool("Moving", false);
-                    UpdateAttack();
-                // }
-
+                animator.SetBool("Moving", false);
+                if (((1 << hit.transform.gameObject.layer) & enemyLayer) != 0)
+                {
+                    StartCoroutine(Attack());
+                }
             }
-        }
-    }
-
-    private void UpdateAttack()
-    {
-        if (!Physics.Raycast(transform.position + Vector3.up * 2.0f, transform.forward, out hitInFront, attackDistance))
-        {
-            if (!animator.GetBool("Moving"))
-                animator.SetBool("Moving", true);
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        }
-        
-        else if (((1 << hitInFront.transform.gameObject.layer) & enemyLayer) != 0)
-        {
-            animator.SetBool("Moving", false);
-            StartCoroutine(Attack());
         }
     }
 
     IEnumerator Attack()
     {
         isAttacking = true;
-        animator.SetBool("Moving", false);
+        animator.SetTrigger("Trigger");
+        float attackAnimationLength = animator.GetCurrentAnimatorClipInfo(0).Length;
+        yield return new WaitForSeconds(attackAnimationLength);
 
-        for (;;)
-        {
-            animator.SetTrigger("Trigger");
-            float attackAnimationLength = animator.GetCurrentAnimatorClipInfo(0).Length;
-            yield return new WaitForSeconds(attackAnimationLength);
-
-            if (!Physics.Raycast(transform.position + Vector3.up * 2.0f, transform.forward, out hitInFront,
-                    attackDistance))
-                break;
-            if (((1 << hitInFront.transform.gameObject.layer) & enemyLayer) != 0) continue;
-            isAttacking = false;
-            break;
-        }
-
+        // RaycastHit hit;
+        // if (Physics.Raycast(transform.position + Vector3.up * 2.0f, transform.forward, out hit, attackDistance))
+        // {
+        //     if (((1 << hit.transform.gameObject.layer) & enemyLayer) != 0)
+        //     {
+        //         StartCoroutine(Attack());
+        //     }
+        // }
 
         isAttacking = false;
     }
@@ -98,7 +77,6 @@ public class SoldierController : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Debug.DrawRay(transform.position + Vector3.up * 2.0f, transform.forward * attackDistance, Color.red);
-        Debug.DrawRay(transform.position + Vector3.up * 2.0f, transform.forward * walkDistance, Color.green);
     }
 
     public void SetParameters(LayerMask layer, float spacing)
